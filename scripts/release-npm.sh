b823597de5485b1eef9034c25919e424d59ebf0d
George B <705427+georgeblahblah@users.noreply.github.com>
2022-10-13 10:40:49 +0100

    Publish to npm on releases

--- scripts/release-npm.sh
+++ scripts/release-npm.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 set -e
+SNAPSHOT=false
+if [[ "$1" == "snapshot" ]] ; then
+    SNAPSHOT=true
+fi
 
 VERSION=$(scripts/get-version.sh)
-sbt "project tsApiModels" "releaseNpm $VERSION"
+
+if $SNAPSHOT ; then
+    sbt "project tsApiModels" "set scroogeTypescriptPublishTag := \"snapshot\"" "releaseNpm $VERSION"
+else
+    sbt "project tsApiModels" "releaseNpm $VERSION"
+fi
