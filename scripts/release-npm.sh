532f271f45dc390d5e7ceb7f0d83f184912191bf
George B <705427+georgeblahblah@users.noreply.github.com>
2022-10-20 14:20:33 +0100

    get snapshot version in CI from git tag

--- scripts/release-npm.sh
+++ scripts/release-npm.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 set -e
+VERSION=$(scripts/get-version.sh)
 SNAPSHOT=false
+
 if [[ "$1" == "snapshot" ]] ; then
     SNAPSHOT=true
+    # If we are in CI and this is a snapshot release, we should get the version from the tag created in the GitHub Release
+    if [[ $CI ]] ; then
+        VERSION=$(git describe --tags)
+    fi
 fi
 
-VERSION=$(scripts/get-version.sh)
 
 if $SNAPSHOT ; then
     sbt "project tsApiModels" "set scroogeTypescriptPublishTag := \"snapshot\"" "releaseNpm $VERSION"
