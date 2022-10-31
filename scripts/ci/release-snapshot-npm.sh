099b5843e23db3eb5f0d6ac085abb5ff881ec6af
George B <705427+georgeblahblah@users.noreply.github.com>
2022-10-31 16:38:23 +0000

    mock the version for testing purposes

--- scripts/ci/release-snapshot-npm.sh
+++ scripts/ci/release-snapshot-npm.sh
@@ -7,7 +7,8 @@ popd
 
 # If we are in CI and this is a snapshot release, we should get the version from the tag created in the GitHub Release
 if [[ $CI ]] ; then
-    VERSION=$(git describe --tags)
+    # VERSION=$(git describe --tags)
+    VERSION="0.0.0-2022-10-31-TEST-SNAPSHOT"
 fi
 
 sbt "project tsApiModels" "set scroogeTypescriptPublishTag := \"snapshot\"" "releaseNpm $VERSION"
